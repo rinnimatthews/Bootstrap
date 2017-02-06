6c2f39d3e4728751c296b3cf4b5a3824199cf221
rinnimatthews <rinnimatthews@gmail.com>
2017-02-06 05:32:02 -0800

    install file

---
+++ install.sh
@@ -0,0 +1,14 @@
+#https://raw.githubusercontent.com/rinnimatthews/ECC-bootstrap/master/bootstrap.sh
+
+#https://raw.githubusercontent.com/rinnimatthews/ECC-bootstrap/master/ECCbootstrap.sh
+
+#Link 1: (SIFT type install)
+
+#wget --quiet -O - https://raw.githubusercontent.com/rinnimatthews/ECC-bootstrap/master/bootstrap.sh | sudo bash -s -- -i -s -y
+
+#Link 2: (Normal ECC Install)
+
+#!/bin/bash
+
+wget --quiet -O - https://raw.githubusercontent.com/rinnimatthews/ECC-bootstrap/master/ECCbootstrap.sh | sudo bash -s -- -i -s -y
+
